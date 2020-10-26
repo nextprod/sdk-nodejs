647ebaedd77c52f0ec2717b8baa1ecd3d6eb680a
Justinas Petrulis <justinas.petrulis@gmail.com>
2020-10-26 12:33:11 +0200

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ async function invoke(call, callback) {
   console.log(call.request.getEvent());
   const handler = require('/root/extension/dist/index.js')
   console.log(handler)
-  await handler.run()
+  await handler.run(call.request.getEvent())
   callback(null, messages.InvokeReply());
 }
 
@@ -19,5 +19,11 @@ function main() {
   server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
   server.start();
 }
-
+// The problem with grpc extensions is that separate docker container with extension
+// added as a volume has to be added or runtime added as a volume to extensions' container.
+// If runtime is added to extension container as a volume, it means that runtime has to be
+// downloaded before executing job. To optimize step execution, runtimes could be downloaded
+// to agents' directory on startup.
+// However for statically typed languages like Golang, where executable binary is produced,
+// runtime should be imported to extension package and compiled together.
 main()
